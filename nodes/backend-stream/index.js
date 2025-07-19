b20639b1aaa1fed8074b5d24f3f921bdb14395a7
zulqurnain mushtaq <zulqurnaincheema45@gmail.com>
2025-07-19 21:53:01 +0500

    Update CORS configuration and server host binding

--- nodes/backend-stream/index.js
+++ nodes/backend-stream/index.js
@@ -38,7 +38,7 @@ const fastify = Fastify({
   logger: loggerOptions,
 });
 fastify.register(fastifyCors, {
-  origin: true, // Adjust this for production (e.g., specific origins)
+  origin: "*", // Adjust this for production (e.g., specific origins)
   methods: ["GET", "POST", "OPTIONS"],
 });
 fastify.register(fastifyFormBody);
@@ -580,7 +580,7 @@ ${customParamters.trainingData}
   });
 });
 
-fastify.listen({ port: PORT,host: "0.0.0.0"  }, (err) => {
+fastify.listen({ port: PORT }, (err) => {
   if (err) {
     logger.error(err);
     process.exit(1);
